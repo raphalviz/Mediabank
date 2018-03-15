d60c7be771482c7c6a40bdfac8c3a185384750a7
aaccstaff <aaccstaff@dhcphost-bv134.utsc.utoronto.ca>
2018-03-15 14:30:50 -0400

    press enter to login

--- js/login.js
+++ js/login.js
@@ -1,9 +1,25 @@
 (function () {
+  var usernameInput = document.getElementById('username-input');
+  var passwordInput = document.getElementById('password-input');
   var btnLogin = document.getElementById('login-button');
 
+  var keyUpLogin = function () {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      btnLogin.click();
+    }
+  }
+  usernameInput.addEventListener("keyup", function(event) {
+    keyUpLogin();
+  });
+
+  passwordInput.addEventListener("keyup", function(event) {
+    keyUpLogin();
+  });
+  
   btnLogin.onclick = function () {
-    var username = document.getElementById('username-input').value;
-    var password = document.getElementById('password-input').value;
+    var username = usernameInput.value;
+    var password = passwordInput.value;
 
     $.post('api/users/read.php', { username, password }, function (res) {
       if (res == "Login Successful.") {
