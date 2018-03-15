c72b1449dc844fee6716c3fad5e4beefcd19e36e
aaccstaff <aaccstaff@dhcphost-bv134.utsc.utoronto.ca>
2018-03-15 13:53:15 -0400

    test redir https

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 (function () {
   var btnLogin = document.getElementById('login-button');
-
+  document.URL = document.URL.replace(/^http:\/\//i, 'https://');
   btnLogin.onclick = function () {
     var username = document.getElementById('username-input').value;
     var password = document.getElementById('password-input').value;
