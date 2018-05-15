12a135d48d96e7356169e03450213c3b46a5ae7a
aaccstaff <aaccstaff@dhcphost-bv217.utsc.utoronto.ca>
2018-05-15 14:32:23 -0400

    backend api docs wip

--- partials/header.php
+++ partials/header.php
@@ -6,6 +6,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Raphael Alviz">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <title>Mediabank</title>
